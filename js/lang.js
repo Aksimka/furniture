d774eaec0edf4494c42f2e563855323bde574a49
Aksimka <36276085+Aksimka@users.noreply.github.com>
2018-10-04 23:40:13 +0300

    correct

--- js/lang.js
+++ js/lang.js
@@ -30,15 +30,15 @@ function lang() {
             decor: 'Декор',
             vids: [
                 {
-                    image: '../images/vid1.png',
+                    image: 'images/vid1.png',
                     desc: 'Инновационный дизайн'
                 },
                 {
-                    image: '../images/vid2.png',
+                    image: 'images/vid2.png',
                     desc: 'Лучшие профессионалы'
                 },
                 {
-                    image: '../images/vid3.png',
+                    image: 'images/vid3.png',
                     desc: 'Качественный продукт'
                 }
             ]
